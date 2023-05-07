8015c1d709db92c52fa5fc17220908a1e6d2d422
ExternalExte <mail@exte.dev>
2023-05-07 09:50:40 +0000

    add invoke function

--- src/invoke.ts
+++ src/invoke.ts
@@ -1,8 +1,18 @@
-import { CloudFunctionsServiceClient, v2 } from '@google-cloud/functions';
+import { GoogleAuth } from 'google-auth-library';
+if (!process.env.FUNCTION_URL)
+  throw Error('Cloud FunctionのURLを"FUNCTION_URL"に指定してください');
+const url = process.env.FUNCTION_URL;
 
-
-const client = new CloudFunctionsServiceClient();
-await client.initialize();
-console.log(await client.getProjectId());
-for await(const res of client.listFunctionsAsync())
-  console.log(res);
+const auth = new GoogleAuth();
+export async function invokeCloudFunction(instanceName: string, boot: 'on' | 'off') {
+  const client = await auth.getIdTokenClient(url);
+  const res = await client.request({
+    method: 'POST',
+    url,
+    data: {
+      target: instanceName,
+      switch: boot
+    }
+  });
+  console.info(res.data);
+}
